28209bf9b1898b0ae2e1734bdd9fdcb9dcd834d0
Aleksandar <stancevac@hotmail.com>
2025-03-18 00:11:03 +0100

    added styles

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -11,7 +11,7 @@ export default async function RootLayout(props: { children: React.ReactNode }) {
 
   return (
     <html lang="en">
-      <body>
+      <body className="dark">
         <main>{children}</main>
       </body>
     </html>
